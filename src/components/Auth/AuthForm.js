cf96b186b05e51d8ef5feac56aeb835f6e94895f
vishnu-mukati <mukativishnu1232@gmaila.com>
2024-12-08 19:58:24 +0530

    sending the sinup request

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -3,26 +3,56 @@ import { useState, useRef } from 'react';
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
   const [isLogin, setIsLogin] = useState(true);
-  const [isLoading,setIsLoading] = useState(false);
+  // const [isLoading,setIsLoading] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
-  const loginPageHandler = (event) =>{
+  const formSubmitHandler = (event) =>{
     event.preventDefault();
-   setIsLoading(true);
+    
+   const enteredEmail = emailInputRef.current.value;
+   const enteredPassword = passwordInputRef.current.value;
+
+    if(isLogin){
+
+    }else{
+      fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA7KB5CBDaD9KLz18uxYk-F85I2UmMThP0',
+        {
+          method:'POST',
+          body:JSON.stringify({
+             email:enteredEmail,
+             password : enteredPassword,
+             returnSecureToken : true,
+          }),
+          headers:{
+            'Content-Type' : 'application/json'
+          }
+        }
+      ).then(res=>{
+        if(res.ok){
+
+        }else{
+          res.json().then((data)=>{
+             console.log(data);
+          })
+        }
+      })
+    }
   }
   
 
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form >
+      <form onSubmit={formSubmitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input type='email' id='email' required  ref={emailInputRef}/>
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
@@ -30,13 +60,14 @@ const AuthForm = () => {
             type='password'
             id='password'
             required
+            ref={passwordInputRef}
           />
         </div>
-        {!isLoading ? 
+        
         <div className={classes.actions}> 
-          <button className={classes.actions} onClick={loginPageHandler}>{isLogin ? 'Login' : 'Sign Up'}</button>
-        </div> : <p className={classes.auth}>Sending request...</p>
-        }
+          <button className={classes.actions} >{isLogin ? 'Login' : 'Create Acount'}</button>
+        </div> 
+        
         <div className={classes.actions}>
           <button
             type='button'
