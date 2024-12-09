839fec1c3a9953024ebecb130fdbe922b4d58cb8
vishnu-mukati <mukativishnu1232@gmaila.com>
2024-12-09 22:06:22 +0530

    adding the sign in page authentication

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -6,45 +6,56 @@ const AuthForm = () => {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
   const [isLogin, setIsLogin] = useState(true);
-  // const [isLoading,setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
-  const formSubmitHandler = (event) =>{
+  const formSubmitHandler = (event) => {
     event.preventDefault();
-    
-   const enteredEmail = emailInputRef.current.value;
-   const enteredPassword = passwordInputRef.current.value;
 
-    if(isLogin){
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
 
-    }else{
-      fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA7KB5CBDaD9KLz18uxYk-F85I2UmMThP0',
-        {
-          method:'POST',
-          body:JSON.stringify({
-             email:enteredEmail,
-             password : enteredPassword,
-             returnSecureToken : true,
-          }),
-          headers:{
-            'Content-Type' : 'application/json'
-          }
+    setIsLoading(true);
+
+    let url;
+    if (isLogin) {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA7KB5CBDaD9KLz18uxYk-F85I2UmMThP0'
+
+    } else {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA7KB5CBDaD9KLz18uxYk-F85I2UmMThP0'
+
+    };
+    fetch(url,{
+        method: 'POST',
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true,
+        }),
+        headers: {
+          'Content-Type': 'application/json'
         }
-      ).then(res=>{
-        if(res.ok){
+      }
+    ).then(res => {
+      setIsLoading(false);
+      if (res.ok) {
+        return res.json();
+      } else {
+        res.json().then((data) => {
+          const errorMessage = 'Authentication error';
+          throw new Error(errorMessage);
+        })
+      }
+    }).then((data) => {
+      console.log(data);
+    }).catch((error) => {
+      alert(error.message);
+    })
+  };
 
-        }else{
-          res.json().then((data)=>{
-             console.log(data);
-          })
-        }
-      })
-    }
-  }
-  
 
   return (
     <section className={classes.auth}>
@@ -52,7 +63,7 @@ const AuthForm = () => {
       <form onSubmit={formSubmitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required  ref={emailInputRef}/>
+          <input type='email' id='email' required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
@@ -63,11 +74,12 @@ const AuthForm = () => {
             ref={passwordInputRef}
           />
         </div>
-        
-        <div className={classes.actions}> 
-          <button className={classes.actions} >{isLogin ? 'Login' : 'Create Acount'}</button>
-        </div> 
-        
+
+        <div className={classes.actions}>
+          {!isLoading && <button className={classes.actions} >{isLogin ? 'Login' : 'Create Acount'}</button>}
+          {isLoading && <p>Sending request...</p>}
+        </div>
+
         <div className={classes.actions}>
           <button
             type='button'
